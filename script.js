e35d18f121af5669644406b071a2e705f171a19f
Nino Gobejishvili <ninogobejishvili@gmail.com>
2023-04-15 18:50:05 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button")
+const errorText = document.querySelector(".error")
+button.addEventListener('click',function(){
+errorText.style.display = 'block'
+errorIcon.style.display = 'inline'})
+
